375a115d6bae536539def3ee864f07db954ddfb5
Jim Albert <albertcb1@gmail.com>
2018-08-11 12:49:15 -0400

    changes to documentation and one function

--- R/half.slope.ratio.R
+++ R/half.slope.ratio.R
@@ -1,6 +1,6 @@
 half.slope.ratio <- function (sx, sy, px, py){
     tx <- (sx ^ px - 1) / px
-    ty - (sy ^ py - 1) / py
+    ty <- (sy ^ py - 1) / py
     b.left <- diff(ty[1 : 2]) / diff(tx[1 : 2])
     b.right <- diff(ty[2 : 3]) / diff(tx[2 : 3])
     b.right / b.left
